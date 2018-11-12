5461948b50a2ee9be13ad212d8411e119c222e77
Shannon Harris <sr60621@gmail.com>
2018-11-12 11:20:14 -0600

    Insert mapboxToken in restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: api_key,
+        mapboxToken: 'pk.eyJ1Ijoic2RyNjA2MjEiLCJhIjoiY2pvZDR1YXAyMG96NTN3bGVseTVlaXc0MSJ9.tCIq1jwD9SIRECTFz0Nv9w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
